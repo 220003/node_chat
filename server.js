6a6cf398c73b00af31ff256af041efe73ac132d0
220003 <220003pb@yse-c.net>
2023-12-13 10:54:58 +0900

    socket.io読み込み

---
+++ server.js
@@ -0,0 +1,27 @@
+//expressの作成
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+//publicフォルダを、web公開
+app.use(express.static(__dirname + '/public'));
+
+//HTTPサーバの作成
+const { createServer } = require('node:http');
+const server = createServer(app);
+
+//環境変数読み込み
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST
+const port = process.env.PORT
+
+//HTTPサーバーを待機
+server.listen(port, host, () => {
+    console.log('server Listen...')
+    console.log(`listening on http://${host}:${port}`);
+    })
+
+    
+
+    
